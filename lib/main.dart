81603b601e49c340e78eb44a16992522932fd84f
Reginaldo Gutter <reginaldogutter@gmail.com>
2020-02-18 03:14:13 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import './read.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'BLE Tecnovix',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter BLE'),
+        routes: {
+          '/read': (ctx) => ReadScreen(),
+        },
+      );
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  final List<BluetoothDevice> devicesList = new List<BluetoothDevice>();
+  final Map<Guid, List<int>> readValues = new Map<Guid, List<int>>();
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _addDeviceTolist(final BluetoothDevice device) {
+    if (!widget.devicesList.contains(device)) {
+      setState(() {
+        widget.devicesList.add(device);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.flutterBlue.connectedDevices
+        .asStream()
+        .listen((List<BluetoothDevice> devices) {
+      for (BluetoothDevice device in devices) {
+        _addDeviceTolist(device);
+      }
+    });
+    widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+      for (ScanResult result in results) {
+        _addDeviceTolist(result.device);
+      }
+    });
+    widget.flutterBlue.startScan();
+  }
+
+  ListView _buildListViewOfDevices() {
+    List<Container> containers = new List<Container>();
+    for (BluetoothDevice device in widget.devicesList) {
+      containers.add(
+        Container(
+          height: 50,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Text(device.name == '' ? '(unknown device)' : device.name),
+                    Text(device.id.toString()),
+                  ],
+                ),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                child: Text(
+                  'Connect',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  widget.flutterBlue.stopScan();
+                  try {
+                    await device.connect();
+                  } catch (e) {
+                    if (e.code != 'already_connected') {
+                      throw e;
+                    }
+                  } finally {
+                    List<BluetoothService> _services =
+                        await device.discoverServices();
+
+                    Navigator.of(context).pushNamed(
+                      '/read',
+                      arguments: {
+                        'services': _services,
+                        'device': device,
+                      },
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+
+  ListView _buildView() {
+    return _buildListViewOfDevices();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: _buildView(),
+      );
+}
---
+++ lib/read.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+const List<int> READ_DATE = [
+  0x80,
+  0x0F,
+  0xF0,
+  0x04,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00
+];
+
+const List<int> READ_DEVICE_ID = [
+  0x80,
+  0x0F,
+  0xF0,
+  0x0C,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00
+];
+
+class ReadScreen extends StatefulWidget {
+  // BluetoothService notifyService = _services.firstWhere(
+  //                       (element) =>
+  //                           element.uuid.toString() ==
+  //                           '0000ffe0-0000-1000-8000-00805f9b34fb');
+
+  //                   BluetoothService writeService = _services.firstWhere(
+  //                       (element) =>
+  //                           element.uuid.toString() ==
+  //                           '0000ffe5-0000-1000-8000-00805f9b34fb');
+
+  //                   BluetoothCharacteristic notifyCaracteristic =
+  //                       notifyService.characteristics.firstWhere((element) =>
+  //                           element.uuid.toString() ==
+  //                           '0000ffe4-0000-1000-8000-00805f9b34fb');
+
+  //                   BluetoothCharacteristic writeCaracteristic =
+  //                       writeService.characteristics.firstWhere((element) =>
+  //                           element.uuid.toString() ==
+  //                           '0000ffe9-0000-1000-8000-00805f9b34fb');
+
+  //                   await notifyCaracteristic.setNotifyValue(true);
+
+  //                   notifyCaracteristic.value.listen((value) {
+  //                     print('====================================');
+  //                     print(value);
+  //                     print('====================================');
+  //                     widget.readValues[notifyCaracteristic.uuid] = value;
+  //                   });
+
+  //                   while (true) {
+  //                     writeCaracteristic.write(READ_DATE);
+
+  //                     await Future.delayed(Duration(seconds: 1));
+  //                   }
+
+  @override
+  _ReadScreenState createState() => _ReadScreenState();
+}
+
+class _ReadScreenState extends State<ReadScreen> {
+  List<BluetoothService> services;
+  BluetoothDevice device;
+  BluetoothService notifyService;
+  BluetoothService writeService;
+  BluetoothCharacteristic notifyCaracteristic;
+  BluetoothCharacteristic writeCaracteristic;
+  List<int> receivedData;
+
+  var _loadedInitData = false;
+  var _isReadingDateTime = false;
+
+  @override
+  void didChangeDependencies() async {
+    if (!_loadedInitData) {
+      final routeArgs =
+          ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
+
+      services = routeArgs['services'];
+      device = routeArgs['device'];
+
+      notifyService = services.firstWhere((element) =>
+          element.uuid.toString() == '0000ffe0-0000-1000-8000-00805f9b34fb');
+
+      writeService = services.firstWhere((element) =>
+          element.uuid.toString() == '0000ffe5-0000-1000-8000-00805f9b34fb');
+
+      notifyCaracteristic = notifyService.characteristics.firstWhere(
+          (element) =>
+              element.uuid.toString() ==
+              '0000ffe4-0000-1000-8000-00805f9b34fb');
+
+      writeCaracteristic = writeService.characteristics.firstWhere((element) =>
+          element.uuid.toString() == '0000ffe9-0000-1000-8000-00805f9b34fb');
+
+      await notifyCaracteristic.setNotifyValue(true);
+
+      notifyCaracteristic.value.listen((value) {
+        print('====================================');
+        print(value);
+        print('====================================');
+
+        receivedData = value;
+      });
+
+      _loadedInitData = true;
+    }
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        await device.disconnect();
+        Navigator.pop(context);
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Choose Action'),
+        ),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                color: Colors.blue,
+                child: Text(
+                  _isReadingDateTime
+                      ? 'Stop Reading Date/Time'
+                      : 'Start Reading Date/Time',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  _isReadingDateTime = !_isReadingDateTime;
+
+                  setState(() {});
+
+                  while (_isReadingDateTime) {
+                    writeCaracteristic.write(READ_DATE);
+                    await Future.delayed(Duration(seconds: 1));
+                  }
+                },
+              ),
+              RaisedButton(
+                color: Colors.blue,
+                child: Text(
+                  'Read Device ID',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  writeCaracteristic.write(READ_DEVICE_ID);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
